87ec6df7895bd4cb0587f76fa47350b4a68e3644
Sofiia Yarmolenko <sophiayarmolenko@gmail.com>
2020-11-02 13:33:47 +0200

    Finish

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -9,8 +9,9 @@ public class Client
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
-    private FileWriter fileWriter = new FileWriter(new File("history.txt"));
+    private FileWriter fileWriter;
 
+    private static final String EXIT = "exit";
     private static final String HOST = "localhost";
     private static final int PORT = 1033;
 
@@ -42,29 +43,41 @@ public class Client
 
     public static void main(String[] args) throws IOException
     {
-        Date date = new Date();
+        Date date;
         Scanner scanner = new Scanner((System.in));
 
         Client client = new Client();
         client.start(HOST, PORT);
-        String word;
-        String language;
+        client.fileWriter = new FileWriter("history.txt");
+        String word = "";
+        String language = "";
 
-        client.fileWriter.append(date.toString() + " > " + "word for translation (English, French, German, Russian, Ukrainian):" );
+        date = new Date();
+        client.fileWriter.write(date.toString() + " > " + "word for translation (English, French, German, Russian, Ukrainian):" +"\n");
         System.out.println("word for translation (English, French, German, Russian, Ukrainian):");
+
         while (scanner.hasNext())
         {
             word = scanner.nextLine();
-            client.fileWriter.append(date.toString() + " > " + word);
+            date = new Date();
+            client.fileWriter.write(date.toString() + " > " + word + "\n");
+            if(word.equals(EXIT))
+                break;
             System.out.println("translation language (English, French, German, Russian, Ukrainian):");
             language = scanner.nextLine();
-            client.fileWriter.append(date.toString() + " > " + language);
+            date = new Date();
+            client.fileWriter.write(date.toString() + " > " + language + "\n");
+            if(language.equals(EXIT))
+                break;
             String serverResult = client.sendWordAndLanguage(word, language);
             System.out.println(serverResult);
-            client.fileWriter.append(date.toString() + " > " + serverResult);
+            date = new Date();
+            client.fileWriter.write(date.toString() + " > " + serverResult+"\n");
             System.out.println("word for translation (English, French, German, Russian, Ukrainian):");
-            client.fileWriter.append(date.toString() + " > " + "word for translation (English, French, German, Russian, Ukrainian):");
+            date = new Date();
+            client.fileWriter.write(date.toString() + " > " + "word for translation (English, French, German, Russian, Ukrainian):"+"\n");
         }
+        client.clientSocket.close();
         client.fileWriter.close();
     }
 }
